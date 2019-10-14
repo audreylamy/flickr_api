354b1d0111e42ed80c6b02daf8ddd7d9f3947a0d
Audrey LAMY <alamy@e3r11p18.42.fr>
2019-10-14 14:17:18 +0200

    .env (API_KEY) + not found

--- src/components/listImg/index.js
+++ src/components/listImg/index.js
@@ -1,32 +1,45 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { WrapperImg } from "./style"
 import Images from "../images/index"
 import { WithPhotosHoc } from "../../hoc/photosHOC"
 import { withRouter } from 'react-router'
 import { WrapperText } from "../linkPages/style";
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 const ListImg = ({ context, history }) => {
 
-	const [allPhotos, setAllPhotos] = React.useState([])
+	const [allPhotos, setAllPhotos] = useState([])
+	const [tagI, setTag] = useState(null)
+	const [loader, setLoader] = useState(true)
 
-	React.useEffect(() => {
-			setAllImages()
+	useEffect(() => {
+			if (history.location.pathname.toLowerCase() === "/cat" || "/dog" || "/bird") {
+				let path = history.location.pathname.split('/')
+				setAllImages(path[1].toLowerCase())
+			}
 	  }, [context.tag]);
 
-	const setAllImages = async () => {
+	const setAllImages = async (tag) => {
+		console.log(tag)
+		setTag(tag)
 		var url;
 		if (history.location.pathname === "/" && context.tag === 'all')
-			url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff9cea982e01046ebef0e6040d1f9b05&tags=all&format=json&nojsoncallback=true'
-		else if (history.location.pathname !== "/")
-			url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff9cea982e01046ebef0e6040d1f9b05&tags=' + context.tag + '&format=json&nojsoncallback=true'
+			url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&tags=all&format=json&nojsoncallback=true'
+		else if (history.location.pathname !== "/" && tag)
+			url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&tags=' + tag + '&format=json&nojsoncallback=true'
 		else
-			url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff9cea982e01046ebef0e6040d1f9b05&tags=' + context.tag + '&format=json&nojsoncallback=true'
+			url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + API_KEY + '&tags=' + context.tag + '&format=json&nojsoncallback=true'
+		console.log(url)
 		await fetch(url)
 		.then(response => response.json())
-		.then(data => setAllPhotos(data.photos.photo));
+		.then(data => {setAllPhotos(data.photos.photo); setLoader(false)})
+		.catch(error => console.log(error))
 	}
 
 		if (allPhotos.length > 0) {
+			console.log(allPhotos)
+			console.log(tagI.length)
 			return (
 				<React.Fragment>
 					<WrapperText>
@@ -45,9 +58,15 @@ const ListImg = ({ context, history }) => {
 					</WrapperImg>
 				</React.Fragment>
 			)
-		} else {
+		} 
+		else if (loader) {
 			return (
 				<div>Loading</div>
+			)
+		}
+		else {
+			return (
+				<div>Not Found</div>
 			)
 		}
 }
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,6 @@
 import React            from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Homepage         from "./Homepage"
-import NotFound          from "./notFound"
 import PhotosProvider   from  "./photosContext"
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
         <PhotosProvider>
           <Route exact path="/" component={Homepage} />
           <Route path="/:tag"component={Homepage} />
-          <Route path="*"component={NotFound} />
         </PhotosProvider>
       </Switch>
     </Router>
